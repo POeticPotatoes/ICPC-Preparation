15a43b17cb961622ad38aa975fb70db11987cfa7
POeticPotatoes <poeticpotatoes@gmail.com>
2022-12-06 22:42:58 +0800

    Update template and notes

--- snippets.md
+++ snippets.md
@@ -1,11 +1,26 @@
 ```c++
-/* Built-in bit functions (prefixed with __builtin_)
+/* 
+ * Built-in bit functions (prefixed with __builtin_)
  * popcount - number of 1's          |  parity - whether the number of 1's is even
- * clz      - number of leading 0's  |  ctz - distance from first to last 1
+ * clz      - number of leading 0's  |  ctz    - distance from first to last 1
+ 
+ * Random
+ * srand((unsigned) time(NULL)) | rand()
+ 
+ * Misc
+ * memset(arr, val, size);
+ * next_permutation(begin, end)
  */
 ```
 
-### TO INCLUDE
+```python
+
+# itertools (import itertools, list(itertools.___))
+# permutations(vll)    - n!          |  chain(item1, item2,...)  - combine items
+# combinations(vll, i) - n choose i  |
+```
+
+## TO INCLUDE
 * Convex hull
 * bfs (weighted/unweighted)
 * dijkstra (2017 manila E)
@@ -18,6 +33,6 @@
 * Chinese remainder theorem?
 * Diophantine
 
-
-ntu39 G, H, L
-ntu38 E, G(maxflow), 
+## To Research
+ntu39: <s>G</s>, H, L  
+ntu38: E, G(maxflow), 
--- template.cpp
+++ template.cpp
@@ -1,57 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define FOR(i, a, b) for (ll (i) = (a); (i) < (b); (i)++)
+#define ROF(i, a, b) for (ll (i) = (a); (i) > (b); (i)--)
+#define all(x) (x).begin(), (x).end()
+#define eb emplace_back
 typedef long long ll;
 typedef long double ld;
-typedef pair<ll, ll> pll;
-template <typename T>
-using t3 = tuple<T,T,T>;
-template <typename T>
-using t4 = tuple<T,T,T,T>;
-typedef vector<ll> vl;
+typedef vector<ll> vll;
 template <typename T>
 using vv = vector<vector<T>>;
 template <typename T>
 using vvv = vector<vv<T>>;
-#define FOR(i, a, b) for (ll (i) = (a); (i) < (b); (i)++)
-#define ROF(i, a, b) for (ll (i) = (a); (i) > (b); (i)--)
-#define REP(i, n) FOR(i, 0, (n))
-#define all(x) (x).begin(), (x).end()
-#define eb emplace_back
-#define em emplace
-#define mp make_pair
-#define um unordered_map
-#define lb_pos(arr, key) lower_bound(all(arr), key) - (arr).begin()
-#define ub_pos(arr, key) upper_bound(all(arr), key) - (arr).begin()
-#define FILL(arr, num) memset(arr, num, sizeof(arr))
-#ifndef ONLINE_JUDGE
-#define dbg(x...) do { cout << "\033[32;1m " << #x << " -> "; err(x); } while (0)
-void err() { cout << "\033[39;0m" << endl; }
-template<template<typename...> class T, typename t, typename... A>
-void err(T<t> a, A... x) { for (auto v: a) cout << v << ' '; err(x...); }
-template<typename T, typename... A>
-void err(T a, A... x) { cout << a << ' '; err(x...); }
-#else
-#define dbg(...)
-#define err(...)
-#endif
+template <typename T, typename N>
+using um = unordered_map<T, N>;
 template <typename T>
 using MinHeap = priority_queue<T, vector<T>, greater<T>>;
 template <typename T>
 using MaxHeap = priority_queue<T>;
-ll MOD = 1e9+7;
-ll M(ll n) {return (n % MOD + MOD) % MOD;}
-mt19937 rng((unsigned int) chrono::steady_clock::now().time_since_epoch().count());
-#define uid(a, b) uniform_int_distribution<int>19(a, b)(rng)
+
+constexpr int MOD = 1e9+7;
+constexpr int inf = (int)1e9;
+constexpr ll INF = 1e18;
 
 void solve() {
     
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
+    IO;
     int t=1;
-    cin >> t;
+    cin >> t; // Comment this out if there are no tests
     while (t--) solve();
 }
